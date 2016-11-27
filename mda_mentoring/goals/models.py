85c31e8137d15ef5b6e76f4aa5bfcfd02b857257
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2016-11-27 12:42:54 +1000

    Add evaluation date to goals and display high and low evaluations

--- mda_mentoring/goals/models.py
+++ mda_mentoring/goals/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from profiles.models import Profile
+from datetime import datetime
 
 # Create your models here.
 class Goal(models.Model):
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, default='A Goal')
+    eval_date = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.name
@@ -19,3 +21,7 @@ class Milestone(models.Model):
 
     def __str__(self):
         return self.name
+
+# class Action(models.Model):
+#     milestone = models.ForeignKey(Milestone, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=200, default="Here's an action to complete my milestone")
--- mda_mentoring/mda_mentoring/settings.py
+++ mda_mentoring/mda_mentoring/settings.py
@@ -115,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-au'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Australia/Brisbane'
 
 USE_I18N = True
 
